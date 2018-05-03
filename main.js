5a21be714ee0f7e15f390eee6ff34b250c5fa21c
Leitino <alessandro.leita89@gmail.com>
2018-05-03 16:17:54 +0200

    proseguendo per la milestone1

--- main.js
+++ main.js
@@ -5,33 +5,33 @@ $(function() {
     method: "GET",
     success:function(data){
       console.log(data)
+      var totvendite = 0
       for (var i = 0; i < data.length; i++) {
-        var venditori = data[i].salesman
-        console.log(venditori)
-      }
+        var vendite = data[i].amount
+        totvendite = totvendite + vendite
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var chart = new Chart(ctx, {
+            // The type of chart we want to create
+            type: 'line',
 
+            // The data for our dataset
+            data: {
+                labels: ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"],
+                datasets: [{
+                    label: "Totale vendite",
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: [totvendite, totvendite, totvendite],
+                }]
+            },
+
+            // Configuration options go here
+            options: {}
+        });
+
+
+      }
+        console.log(totvendite)
     }
   });
-
-var ctx = document.getElementById('myChart').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'line',
-
-    // The data for our dataset
-    data: {
-        labels: ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Novembre", "Dicembre"],
-        datasets: [{
-            label: "Vendite totali",
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 2000, 45, 20, 15, 23, 40, 8],
-        }]
-    },
-
-    // Configuration options go here
-    options: {}
 });
-
-
-})
